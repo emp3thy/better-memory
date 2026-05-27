672dc80fe0b3e68187429ae5c9da92717c42f93f
gethin <emp3thy@googlemail.com>
2026-05-27 09:40:43 +0100

    style(cli): use collections.abc.Sequence instead of typing.Sequence

--- better_memory/cli/main.py
+++ better_memory/cli/main.py
@@ -8,7 +8,7 @@ from __future__ import annotations
 
 import argparse
 import sys
-from typing import Sequence
+from collections.abc import Sequence
 
 
 def _build_parser() -> argparse.ArgumentParser:
